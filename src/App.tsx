bb977e5de9494ddf81ef5402a6cb15bd7628b8f9
Nick <qiujiahongde@163.com>
2020-11-22 17:36:28 +0800

    添加一些样式演示

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <h1>hello word</h1>
+        <h2>hello word</h2>
+        <h3>hello word</h3>
+        <hr />
+        <code>
+          const a = 'b'
+        </code>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
